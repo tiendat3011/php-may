1813979cb0357f91b8b489ba03d518639a7a94f7
TienDat <tiendat30111999@gmail.com>
2024-06-06 00:58:00 +0700

    using ckfinder

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -14,8 +14,8 @@ class ExampleTest extends TestCase
      */
     public function test_example()
     {
-        $response = $this->get('/');
+        $response = $this->get('http://127.0.0.1:8000/');
 
-        $response->assertStatus(200);
+        $response->assertStatus(302);
     }
 }
